9954939d8371657dda62dd692c2eae6eefd53d76
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-10-22 22:47:50 +0530

    Refactor App component to use useState for count

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [count, setCount] = useState(0);
 
-const App = () => {
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div className="App-header">
+        <p>Button clicked {count} times</p>
+        <button onClick={handleClick}>
+          Click me
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
