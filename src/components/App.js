9a4ea9f3d2fde2792df766e937eee355a0f1a668
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-10-23 11:49:54 +0530

    Refactor App component and update imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,15 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import React,{useState} from "react";
+import './../styles/App.css';
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
+const App = () => {
+  const [count,setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App-header">
+    <div>
         <p>Button clicked {count} times</p>
-        <button onClick={handleClick}>
-          Click me
-        </button>
-      </div>
+    <button onClick={()=>setCount(count+1)}>Click me</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
